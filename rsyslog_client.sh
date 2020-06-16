f55c848e71ad7208afc0b86cb51288d1a0c6e5ff
Aliya <22922673+aliyacodes@users.noreply.github.com>
2020-06-15 17:00:39 -0700

    Create rsyslog_client.sh

---
+++ rsyslog_client.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "*.info;mail.none;authpriv.none;cron.none   @rsyslog" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+#Important: this should be the internal not external IP of the server or the dns name of your server.
