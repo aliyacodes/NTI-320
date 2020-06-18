a1ecad6c33c619952674cc6e80a98eb6fbe85e19
Aliya <22922673+aliyacodes@users.noreply.github.com>
2020-06-18 16:21:53 -0700

    Update rsyslog_server.sh

--- rsyslog_server.sh
+++ rsyslog_server.sh
@@ -11,6 +11,16 @@ name=NTI310 BASE
 baseurl=http://104.197.59.12/base
 gpgcheck=0
 enabled=1" >> /etc/yum.repos.d/local-repo.repo
+echo "[nti-310-extras]
+name=NTI310 EXTRAS
+baseurl=http://104.197.59.12/extras/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+echo "[nti-310-updates]
+name=NTI310 UPDATES
+baseurl=http://104.197.59.12/updates/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
 
 sed -i 's/#$UDPServerRun 514/$UDPServerRun 514/g' /etc/rsyslog.conf
 sed -i 's/#$InputTCPServerRun 514/$InputTCPServerRun 514/g' /etc/rsyslog.conf
